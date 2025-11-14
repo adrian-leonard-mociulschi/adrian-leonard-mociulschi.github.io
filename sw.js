196e2402c43b7ab15948b165dbebefe844da28aa
Adrian Leonard Mociulschi <235957329+adrian-leonard-mociulschi@users.noreply.github.com>
2025-11-14 02:06:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — Adrian
-const CACHE_NAME = 'adi-site-cache-v6'; // bump când faci modificări
+const CACHE_NAME = 'adi-site-cache-v7'; // bump când faci modificări
 const PRECACHE_URLS = [
   '/', '/index.html', '/about.html', '/writings.html', '/op-ed.html', '/vbox.html',
   // The "/assets/style.css" file should not be included here.
