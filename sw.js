dfb60d910bc6405c0364c4cb1e3617368f6a86b5
Adrian Leonard Mociulschi <235957329+adrian-leonard-mociulschi@users.noreply.github.com>
2025-11-24 20:19:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Improved Version for GitHub Pages
 // VERSION bump for cache-busting
-const VERSION = 'v38';
+const VERSION = 'v39';
 const CACHES = {
   pages:  `adi-pages-${VERSION}`,
   assets: `adi-assets-${VERSION}`,
