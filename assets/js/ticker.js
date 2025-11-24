bf61cea4170abeedca20f9703b193b99b976132b
Adrian Leonard Mociulschi <235957329+adrian-leonard-mociulschi@users.noreply.github.com>
2025-11-24 18:19:47 +0200

    Update ticker.js

--- assets/js/ticker.js
+++ assets/js/ticker.js
@@ -91,7 +91,7 @@
 
     const fallbackFn = () => {
       setTickerText('.ticker-red', 'Nov 18: România Liberă – Shadows Over the Black Sea: The silent front where Europe’s future is decided.');
-      setTickerText('.ticker-yellow', 'Nov 16: Contributors – Noah’s Ark in the Age of Red Alerts. How do we rebuild trust without sacrificing clarity?');
+      setTickerText('.ticker-yellow', 'Nov 20: Revista Cultura – AI Revolution: Between Hope and Fears. ISF International Conference');
     };
     if ('requestIdleCallback' in window) {
       requestIdleCallback(fallbackFn, { timeout: 500 });
